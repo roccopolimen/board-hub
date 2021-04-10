cc8ce254edd87a430f6ca30d0d923effdde2bfc3
Marco Polimeni <stats3434@gmail.com>
2021-04-10 15:10:33 -0400

    Add boards data w/o error handling

---
+++ src/data/boards.js
@@ -0,0 +1,187 @@
+const mongoCollections = require('../config/mongoCollections');
+const boards = mongoCollections.boards;
+const users = mongoCollections.users;
+const {colors} = require('../public/constants/index');
+const {ObjectId} = require('mongodb');
+
+function newFields(newBoard, oldBoard) {
+  fields = {};
+  if(newBoard.boardName && newBoard.boardName !== oldBoard.boardName) {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    fields.boardName = newBoard.boardName;
+  }
+  if(newBoard.boardColor && newBoard.boardColor !== oldBoard.boardColor) {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    fields.boardColor = newBoard.boardColor;
+  }
+  if(newBoard.description && newBoard.description !== oldBoard.description) {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    fields.description = newBoard.description;
+  }
+
+  return fields;
+}
+
+
+module.exports = {
+  /**
+   * Get a board with a specific ID.
+   * @param {String} id The board's id.
+   * @returns A board.
+   */
+  readByID: async (id) => {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    const boardCollection = await boards();
+    const board = await boardCollection.findOne({_id: ObjectId(id)});
+    if(board === null)
+      throw new Error("There is no board with that id.");
+
+    board['_id'] = board['_id'].toString();
+
+    return board;
+  },
+
+  /**
+   * Gets all boards that a user is a part of.
+   * @param {String} id The user's id.
+   * @returns A list of board objects.
+   */
+  readAll: async (id) => {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    const userCollection = await users();
+    const user = await userCollection.findOne({_id: ObjectId(id)});
+    if(user === null)
+      throw new Error("There is no user with that id.");
+    const boardList = user['boards'];
+
+    return boardList;
+  },
+
+  /**
+   * Adds a new board to the collection. Picks a random color from pre-approved values, and starts the description, lists, and cards as empty.
+   * @param {String} userId The ID of the user requesting to create a new board.
+   * @param {String} boardName The name of the new board.
+   * @returns The board object.
+   */
+  create: async (userId, boardName) => {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    const boardColor = colors[Math.floor(Math.random() * colors.length)];
+    const description = "";
+    const members = [userId];
+    const lists = [];
+    const cards = [];
+
+    let newBoard = {
+      boardName,
+      boardColor,
+      description,
+      members,
+      lists,
+      cards
+    };
+
+    const boardCollection = await boards();
+    const insertInfo = await boardCollection.insertOne(newBoard);
+    if(insertInfo.insertedCount === 0)
+      throw new Error("Could not add board.");
+
+    return await module.exports.readByID(insertInfo.insertedId.toString());
+  },
+
+  /**
+   * Updates the board with the fields of boardData from a PUT request.
+   * @param {String} id The board's id.
+   * @param {Object} boardData An object that may contain boardName, boardColor, and description.
+   * @returns A board object with updated fields.
+   */
+  update: async (id, boardData) => {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    const boardCollection = await boards();
+    let updatedBoard = newFields(boardData, await module.exports.readByID(id));
+    if(Object.keys(updatedBoard).length === 0)
+      console.log("No fields to update.");
+    
+    if(updatedBoard.boardName) {
+      await boardCollection.updateOne({ _id: ObjectId(id) },
+        { $set: {boardName: updatedBoard.boardName} });
+    }
+    if(updatedBoard.boardColor) {
+      await boardCollection.updateOne({ _id: ObjectId(id) },
+        { $set: {boardColor: updatedBoard.boardColor} });
+    }
+    if(updatedBoard.description) {
+      await boardCollection.updateOne({ _id: ObjectId(id) },
+        { $set: {description: updatedBoard.description} });
+    }
+
+    return await module.exports.readByID(id);
+  },
+
+  /**
+   * Adds another user as a member to this board.
+   * @param {String} id The board's id.
+   * @param {String} userEmail The unique email of the user to be added.
+   * @returns A board object with updated fields.
+   */
+  addNewMember: async (id, userEmail) => {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    // Add member limit ?????
+
+    // Get the user for their id, and make sure they exist.
+    const userCollection = await users();
+    const user = await userCollection.findOne({email: userEmail});
+    if(user === null)
+      throw new Error("There is no user with that email.");
+
+    // Update the members with that user's id.
+    const boardCollection = await boards();
+    await boardCollection.updateOne({ _id: ObjectId(id) },
+          { $addToSet: {members: user._id.toString()} });
+
+    return await module.exports.readByID(id);
+  },
+
+  /**
+   * Delete the board id from each member's list of board's and drop the board from the boards collection.
+   * @param {String} id The board's id.
+   * @returns A success object.
+   */
+  delete: async (id) => {
+    /**
+     * TODO - ERROR CHECKING
+     */
+    const boardCollection = await boards();
+
+    // Remove the board ID from each member
+    const board = await module.exports.readByID(id);
+    const members = board['members'];
+    const userCollection = await users();
+    for(member of members) {
+      await userCollection.updateOne({ _id: ObjectId(member) },
+        { $pull: { boards: id } });
+    }
+
+    // Delete the board from the collection
+    const deletionInfo = await boardCollection.deleteOne({ _id: ObjectId(id) });
+    if (deletionInfo.deletedCount === 0)
+      throw new Error(`Could not delete board with id of ${id}`);
+
+    return { boardId: id, deleted: true };
+  }
+}
---
+++ src/data/index.js
@@ -0,0 +1,14 @@
+// const userData = require('./users');
+const boardData = require('./boards');
+// const listData = require('./lists');
+// const cardData = require('./cards');
+// const labelData = require('./labels');
+// const commentData = require('./comments');
+
+/**
+ * Un-comment the data above and add to exports below to work on that data.
+ */
+
+module.exports = {
+  boards: boardData
+};
