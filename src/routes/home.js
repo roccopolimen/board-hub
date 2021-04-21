83738031e3747d10ba58be9ca34b1004be5f941a
Brian Wormser <bwormser41@gmail.com>
2021-04-20 20:36:11 -0400

    Login/Sign up modals now properly show x button to close modal

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/', async (req, res) => {
   try {
-    res.sendFile(path.resolve('../static/show_finder.html'));
+    res.sendFile(path.resolve('../src/static/home.html'));
   } catch (e) {
-    res.status(404).render('error-page', { title: "404 Page Not Found", error: true });
+    res.status(404).render('error-page', { title: e, error: true });
   }
 });
 
