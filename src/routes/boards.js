edbcd38865fd3306ea200f5eff09b50dc349018a
Jackson Perry <jackperry2187@gmail.com>
2021-04-24 22:57:42 -0400

    Resolved noted issues

--- src/routes/boards.js
+++ src/routes/boards.js
@@ -7,26 +7,18 @@ const boardsData = require('../data/boards');
  */
 router.get('/', async (req, res) => {
     try {
-        //check if logged in
-        if(req.session.user) {
-            let userId = req.session.user.id;
-            if(userid) {
-                //read all of a user's boards
-                let boards = await boardsData.readAll(userId);
-                //render the boards page with their boards
-                res.render('boards', { title: "Boards", theBoards: boards, loggedIn: true });
-            }
-            else throw "Logged in user must have an ID";
+        let userId = req.session.user._id;
+        if(userid) {
+            //read all of a user's boards
+            let boards = await boardsData.readAll(userId);
+            //render the boards page with their boards
+            res.render('boards', { title: "Boards", theBoards: boards, loggedIn: true });
         }
-        else {
-            //not logged in, should go to index
-            res.redirect('/');
-            return;
-        }
+        else throw new Error("Logged in user must have an ID");
     } 
     catch (error) {
         //Either boardsData.readAll() failed or they were logged in and didn't have an ID (shouldn't happen)
-        res.render('boards', { title: "Boards Error", error: error });
+        res.status(500).render('error-page', { title: "500 Internal Server Error", error: true });
     }
 });
 
