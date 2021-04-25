a9a74af8e788c1d20c96522c9249862267b8d3c1
Rocco Polimeni <rpolimen@stevens.edu>
2021-04-25 13:30:57 -0400

    change seeder file to store ObjectIds instead of strings

--- src/tasks/seed.js
+++ src/tasks/seed.js
@@ -125,23 +125,23 @@ const seedDB = async () => {
     const listOfUsers = [];
 
     // user 1
-    const user1 = makeUser('608424a979077f0a237f1d16', 'user1@gmail.com', 'First', 'User', '#aa47bc', '$2y$08$X1b4xs.OBrhiUxelDZkVdOWNd3ytLWwM8GpmP2CVX490GR/gzjnYm');
+    const user1 = makeUser(ObjectId('608424a979077f0a237f1d16'), 'user1@gmail.com', 'First', 'User', '#aa47bc', '$2y$08$X1b4xs.OBrhiUxelDZkVdOWNd3ytLWwM8GpmP2CVX490GR/gzjnYm');
 
     // user 2
-    const user2 = makeUser('608424a979077f0a237f1d17', 'user2@gmail.com', 'Second', 'User', '#7a1fa2', '$2y$08$.CJDbmU9se5Pzy7L.U4u4er5MF8mcxoA9hvkA/8.Sf4m1KT/7j1K.');
+    const user2 = makeUser(ObjectId('608424a979077f0a237f1d17'), 'user2@gmail.com', 'Second', 'User', '#7a1fa2', '$2y$08$.CJDbmU9se5Pzy7L.U4u4er5MF8mcxoA9hvkA/8.Sf4m1KT/7j1K.');
 
     // user 3
-    const user3 = makeUser('608424a979077f0a237f1d18', 'user3@gmail.com', 'Third', 'User', '#5c6bc0', '$2y$08$mULdDOEWEMtUnKkU6.yPQ.3AAK5qGx34WlDUVyj5StHxJw7dUpF46');
+    const user3 = makeUser(ObjectId('608424a979077f0a237f1d18'), 'user3@gmail.com', 'Third', 'User', '#5c6bc0', '$2y$08$mULdDOEWEMtUnKkU6.yPQ.3AAK5qGx34WlDUVyj5StHxJw7dUpF46');
 
     // user 4
-    const user4 = makeUser('608424a979077f0a237f1d19', 'user4@gmail.com', 'Fourth', 'User', '#689f39', '$2y$08$/oFNbdJN4m/obzbo9znU..1u2OlVsQn2r0yne2ouQJfOiXRnLBLVO');
+    const user4 = makeUser(ObjectId('608424a979077f0a237f1d19'), 'user4@gmail.com', 'Fourth', 'User', '#689f39', '$2y$08$/oFNbdJN4m/obzbo9znU..1u2OlVsQn2r0yne2ouQJfOiXRnLBLVO');
 
 // CREATE BOARDS
 
     const listOfBoards = [];
 
     // BOARD 1
-    const board1 = makeBoard('608424a979077f0a237f1d1a', 'First Board', '#E27D60', 'this is my first board');
+    const board1 = makeBoard(ObjectId('608424a979077f0a237f1d1a'), 'First Board', '#E27D60', 'this is my first board');
 
     addBoardToUser(user1, board1._id);
     addBoardToUser(user2, board1._id);
@@ -149,54 +149,54 @@ const seedDB = async () => {
     addMemberToBoard(board1, user2._id);
 
     // BOARD 1 LISTS
-    const board1List1 = makeList('608424a979077f0a237f1d1b', 'First List');
-    const board1List2 = makeList('608424a979077f0a237f1d1c', 'Second List');
-    const board1List3 = makeList('608424a979077f0a237f1d1d', 'Third List');
+    const board1List1 = makeList(ObjectId('608424a979077f0a237f1d1b'), 'First List');
+    const board1List2 = makeList(ObjectId('608424a979077f0a237f1d1c'), 'Second List');
+    const board1List3 = makeList(ObjectId('608424a979077f0a237f1d1d'), 'Third List');
 
     // BOARD 1 CARD 1
-    const board1Card1 = makeCard('608424a979077f0a237f1d1e', 'Card 1', 1, 'this is my first card', { date: '04/24/2021*12:04', done: false }, board1List1._id);
+    const board1Card1 = makeCard(ObjectId('608424a979077f0a237f1d1e'), 'Card 1', 1, 'this is my first card', { date: '04/24/2021*12:04', done: false }, board1List1._id);
     addCardToList(board1List1, board1Card1._id);
     addCardToBoard(board1, board1Card1);
 
-    const board1Card1Label1 = makeLabel('608424a979077f0a237f1d22', 'label 1', '#3FEEE6');
+    const board1Card1Label1 = makeLabel(ObjectId('608424a979077f0a237f1d22'), 'label 1', '#3FEEE6');
     addLabelToCard(board1Card1, board1Card1Label1);
 
-    const board1Card1Label2 = makeLabel('608424a979077f0a237f1d23', 'label 2', '#EFE2BA');
+    const board1Card1Label2 = makeLabel(ObjectId('608424a979077f0a237f1d23'), 'label 2', '#EFE2BA');
     addLabelToCard(board1Card1, board1Card1Label2);
 
-    const board1Card1Comment1 = makeComment('608424a979077f0a237f1d24', user1._id, '04/08/2020*14:44', 'this is my first comment');
+    const board1Card1Comment1 = makeComment(ObjectId('608424a979077f0a237f1d24'), user1._id, '04/08/2020*14:44', 'this is my first comment');
     addCommentToCard(board1Card1, board1Card1Comment1);
 
-    const board1Card1Comment2 = makeComment('608424a979077f0a237f1d25', user2._id, '09/09/2020*08:08', 'this is my second comment');
+    const board1Card1Comment2 = makeComment(ObjectId('608424a979077f0a237f1d25'), user2._id, '09/09/2020*08:08', 'this is my second comment');
     addCommentToCard(board1Card1, board1Card1Comment2);
 
     // BOARD 1 CARD 2
-    const board1Card2 = makeCard('608424a979077f0a237f1d1f', 'Card 2', 2, 'this is my second card', { date: '04/25/2021*01:05', done: false }, board1List1._id);
+    const board1Card2 = makeCard(ObjectId('608424a979077f0a237f1d1f'), 'Card 2', 2, 'this is my second card', { date: '04/25/2021*01:05', done: false }, board1List1._id);
     addCardToList(board1List1, board1Card2._id);
     addCardToBoard(board1, board1Card2);
 
-    const board1Card2Label1 = makeLabel('608424a979077f0a237f1d26', 'card2Label', '#14A76C');
+    const board1Card2Label1 = makeLabel(ObjectId('608424a979077f0a237f1d26'), 'card2Label', '#14A76C');
     addLabelToCard(board1Card2, board1Card2Label1);
 
-    const board1Card2Comment1 = makeComment('608424a979077f0a237f1d27', user2._id, '06/14/2020*08:15', 'second card comment');
+    const board1Card2Comment1 = makeComment(ObjectId('608424a979077f0a237f1d27'), user2._id, '06/14/2020*08:15', 'second card comment');
     addCommentToCard(board1Card2, board1Card2Comment1);
 
     addAssignedToCard(board1Card2, user2._id);
     addAssignedToCard(board1Card2, user1._id);
 
     // BOARD 1 CARD 3
-    const board1Card3 = makeCard('608424a979077f0a237f1d20', 'Card 3', 3, 'this is my third card', { date: '04/26/2021*01:06', done: true }, board1List1._id);
+    const board1Card3 = makeCard(ObjectId('608424a979077f0a237f1d20'), 'Card 3', 3, 'this is my third card', { date: '04/26/2021*01:06', done: true }, board1List1._id);
     addCardToList(board1List1, board1Card3._id);
     addCardToBoard(board1, board1Card3);
 
     addAssignedToCard(board1Card3, user1._id);
 
     // BOARD 1 CARD 4
-    const board1Card4 = makeCard('608424a979077f0a237f1d21', 'Card 4', 76, 'this is my fourth card', { date: '07/07/2021*01:06', done: true }, board1List2._id);
+    const board1Card4 = makeCard(ObjectId('608424a979077f0a237f1d21'), 'Card 4', 76, 'this is my fourth card', { date: '07/07/2021*01:06', done: true }, board1List2._id);
     addCardToList(board1List2, board1Card4._id);
     addCardToBoard(board1, board1Card4);
 
-    const board1Card4Comment1 = makeComment('608424a979077f0a237f1d28', user2._id, '05/05/2020*05:05', 'looks good!');
+    const board1Card4Comment1 = makeComment(ObjectId('608424a979077f0a237f1d28'), user2._id, '05/05/2020*05:05', 'looks good!');
     addCommentToCard(board1Card4, board1Card4Comment1);
 
     addListToBoard(board1, board1List1);
