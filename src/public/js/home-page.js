5001d1f725b68507c46123ea22619daaa930c768
Marco Polimeni <stats3434@gmail.com>
2021-05-01 17:34:26 -0400

    Preliminary error handling for login/signup

---
+++ src/public/js/home-page.js
@@ -0,0 +1,62 @@
+(function($) {
+    
+    let login_form = $('#login-form');
+    login_form.submit(function(event) {
+        event.preventDefault();
+
+        // TODO - Error Checking
+
+        let email = $('#email').val();
+        let password = $('#password').val();
+        if(email && (email = email.trim()) !== '' &&
+         password && (password = password.trim()) !== '') { // input exists
+
+            let requestConfig = {
+                method: 'POST',
+                url: '/user/login',
+                contentType: 'application/json',
+                data: JSON.stringify({email, password})
+                };
+            $.ajax(requestConfig).then(function(responseMessage) {
+                let error = $(responseMessage)[0];
+                if(error.error)
+                    $('#login-error').show();
+            });
+         } else { // inputs don't exist
+            $('#login-error').show();
+         }
+    });
+
+    let signup_form = $('#signup-form');
+    signup_form.submit(function(event) {
+        event.preventDefault();
+
+        // TODO - Error Checking
+
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let email = $('#signEmail').val();
+        let re_email = $('#verEmail').val();
+        let password = $('#signPassword').val();
+        let re_password = $('#verPassword').val();
+        if(firstName && (firstName = firstName.trim()) !== '' &&
+            lastName && (lastName = lastName.trim()) !== '' &&
+            email && (email = email.trim()) !== '' && email === re_email.trim() &&
+            password && (password = password.trim()) !== '' && password === re_password.trim()) { // input exists
+
+            let requestConfig = {
+                method: 'POST',
+                url: '/user/signup',
+                contentType: 'application/json',
+                data: JSON.stringify({firstName, lastName, email, password})
+                };
+            $.ajax(requestConfig).then(function(responseMessage) {
+                let error = $(responseMessage)[0];
+                if(error.error)
+                    $('#signup-error').show();
+            });
+         } else { // inputs don't exist
+            $('#signup-error').show();
+         }
+    });
+})(window.jQuery);
