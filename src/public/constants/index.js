2ed89ddc20fb2ad55c24bc1cf2c3a44dd8fa779f
Michael Karsen <ryan280@LAPTOP-184O1N5E.localdomain>
2021-05-02 16:42:33 -0400

    html for board settings

--- src/public/constants/index.js
+++ src/public/constants/index.js
@@ -24,7 +24,7 @@ user_colors = ['#aa47bc', '#7a1fa2', '#78909c', '#465a65',
                '#7e57c2', '#512da7', '#ef6c00', '#f5511e',
                '#bf360c'];
 
-
+// Colors for labels
 label_colors = ['#61bd4f', '#f2d600', '#ff9f1a', '#eb5a46', 
                 '#c377e0', '#0079bf', '#00c2e0', '#51e898',
                 '#ff78cb', '#344563'];
