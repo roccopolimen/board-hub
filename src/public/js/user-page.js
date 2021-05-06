27bbae0eec87c6b173f70d7d7c3a85e0ec8977c4
Marco Polimeni <stats3434@gmail.com>
2021-05-06 13:18:01 -0400

    Signout/Delete account btn functionality

--- src/public/js/user-page.js
+++ src/public/js/user-page.js
@@ -1,5 +1,6 @@
 (function($) {
     
+    // Change Password
     let pass_form = $('#change-pass-form');
     pass_form.submit(function(event) {
         event.preventDefault();
@@ -28,4 +29,40 @@
             $('#change-pass-error').show();
          }
     });
+
+
+    // Sign Out
+    let signout = $("#sign-out");
+    signout.click(function(event) {
+        event.preventDefault();
+
+        let requestConfig = {
+            method: 'GET',
+            url: '/users/signout',
+            contentType: 'application/json',
+            data: JSON.stringify({})
+            };
+        $.ajax(requestConfig).then(function(responseMessage) {
+            window.location.href = "/";
+        });
+
+    });
+
+    // Delete Account
+    let delete_acc = $("#delete-account");
+    delete_acc.click(function(event) {
+        event.preventDefault();
+
+        let requestConfig = {
+            method: 'POST',
+            url: '/users/delete',
+            contentType: 'application/json',
+            data: JSON.stringify({})
+            };
+        $.ajax(requestConfig).then(function(responseMessage) {
+            window.location.href = "/";
+        });
+
+    });
+
 })(window.jQuery);
